65dfbe76f8e899c357a3baa84c1fe26b2664dd6f
Estevamsl <estevamsouzalaureth@gmail.com>
2020-05-10 14:58:25 -0300

    Diretório contendo a interface do __app__.py

---
+++ __interfaces__/interface__.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+
+class interface(ABC):
+    # @abstractmethod
+    def somar(self, __n1__: int, __n2__: int) -> any: pass
+    # @abstractmethod
+    def subtrair(self, __n1__: int, __n2__: int) -> any: pass
+    # @abstractmethod
+    def produto(self, __n1__: int, __n2__: int) -> any: pass
+    # @abstractmethod
+    def dividir(self, __n1__: float, __n2__: float) -> any: pass
