c3c9ca0aa9f4c5c52593a9b2606015aefba377c7
Estevamsl <estevamsouzalaureth@gmail.com>
2020-05-10 14:51:30 -0300

    Arquivo Principal

--- sql/database/__Local__ Session.sql
+++ sql/database/__Local__ Session.sql
@@ -1,36 +1,40 @@
 use python;
 
-create table Soma(
-	id int primary key not null unique auto_increment,
+create table Soma
+(
+    -- id int primary key not null unique auto_increment,
     number1 int not null,
     number2 int not null,
-    
+
     Soma int not null
 );
 
-create table Subtracao(
-	id int primary key not null unique auto_increment,
+create table Subtracao
+(
+    -- id int primary key not null unique auto_increment,
     number1 int not null,
     number2 int not null,
-    
+
     Subtracao int not null
 );
 
-create table Produto(
-	id int primary key not null unique auto_increment,
+create table Produto
+(
+    -- id int primary key not null unique auto_increment,
     number1 int not null,
     number2 int not null,
-    
+
     Produto int not null
 );
 
-create table Divisao(
-	id int primary key not null unique auto_increment,
+create table Divisao
+(
+    -- id int primary key not null unique auto_increment,
     number1 int not null,
     number2 int not null,
-    
+
     Divisao dec
-(10, 2) not null
+    -- (10, 2) not null
 );
 
 drop table Soma;
